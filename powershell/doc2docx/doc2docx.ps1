a862d57bf646d579b46a160c212ccbd420e8c346
pea-sys <pea98258@gmail.com>
2022-12-12 06:59:29 +0900

    (doc2docx)wordファイルをReadOnlyで開く

--- powershell/doc2docx/doc2docx.ps1
+++ powershell/doc2docx/doc2docx.ps1
@@ -6,11 +6,10 @@ $wdFormatDocumentDefault = 16
  
 $word = New-Object -ComObject Word.Application
      
-foreach ($f in $files)
-{
+foreach ($f in $files) {
     Write-Host $f
 
-    $doc = $word.Documents.Open($f.FullName)
+    $doc = $word.Documents.Open($f.FullName, $false, $true)
 
     $outputfile = $f.FullName + "x"
     Write-Host $outputfile
