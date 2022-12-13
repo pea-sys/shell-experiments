fe4d375db52f4b5acfb5e00d76066e3c51f99776
pea-sys <pea98258@gmail.com>
2022-12-13 22:01:37 +0900

    (xls2xlsx)powershellの種類の違いにより発生するエラーの修正

--- powershell/xls2xlsx/xls2xlsx.ps1
+++ powershell/xls2xlsx/xls2xlsx.ps1
@@ -8,7 +8,7 @@ Write-Host $tempDir.FullName
 $exlFormatDocumentDefault = 51
  
 $excel = New-Object -ComObject Excel.Application
-$excel.DisplayAlerts = false
+$excel.DisplayAlerts = $false
 
 
 foreach ($f in $files) {
@@ -18,13 +18,12 @@ foreach ($f in $files) {
     $outputfile = $f.Name + "x"
     $outputfile = Join-Path $tempDir.FullName $outputfile
     Write-Host $outputfile
-    # workbooks・ｽ・ｽSaveAs・ｽ・ｽ・ｽT・ｽu・ｽt・ｽH・ｽ・ｽ・ｽ_・ｽz・ｽ・ｽ・ｽﾌ禁止・ｽ・ｽ・ｽ・ｽ・ｽ・ｽ・ｽ・ｽG・ｽX・ｽP・ｽ[・ｽv・ｽﾅゑｿｽ・ｽﾈゑｿｽ・ｽ・ｽ・ｽﾟ、・ｽG・ｽX・ｽP・ｽ[・ｽv・ｽﾂ能・ｽﾈコ・ｽs・ｽ[・ｽ・ｽp・ｽ・ｽ・ｽﾄ間接的・ｽﾉ格・ｽ[
     $books.SaveAs([ref]$outputfile.ToString(), [ref]$exlFormatDocumentDefault)
     $dist = $f.FullName + "x"
     Copy-Item -LiteralPath $outputfile -Destination $dist -Force
     $books.Close()
 }
-$excel.DisplayAlerts = true
+$excel.DisplayAlerts = $true
 $excel.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
 $excel = $null
